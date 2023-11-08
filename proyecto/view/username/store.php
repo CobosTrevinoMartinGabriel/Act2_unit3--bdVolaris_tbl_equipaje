a54e6dbfde2a3361e9084e843a9bb5e08056166e
CobosTrevinoMartinGabriel <143775254+CobosTrevinoMartinGabriel@users.noreply.github.com>
2023-11-08 11:58:24 -0700

    Update store.php

--- proyecto/view/username/store.php
+++ proyecto/view/username/store.php
@@ -1,7 +1,7 @@
 <?php
     require_once("c://xampp/htdocs/proyecto/controller/usernameController.php");
     $obj = new usernameController();
-    $obj->guardar($_POST['idCliente'],$_POST['idVuelo'],$_POST['Peso_gr'],$_POST['tipoEquipaje'],$_POST['size'],$_POST['equipajeMano'],$_POST['pesoMano']);
+    $obj->guardar($_POST["id"],$_POST['idCliente'],$_POST['idVuelo'],$_POST['Peso_gr'],$_POST['tipoEquipaje'],$_POST['size'],$_POST['equipajeMano'],$_POST['pesoMano']);
 
     
     
